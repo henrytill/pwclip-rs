d127772e8b376881097bb5988d939fdd3aa06938
Henry Till <henrytill@gmail.com>
2018-12-21 03:06:28 -0500

    refactor to use serde{,_derive}

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ extern crate clear_on_drop;
 extern crate data_encoding;
 extern crate hmac_drbg;
 extern crate rust_scrypt;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate sha2;
 extern crate toml;
 extern crate typenum;
@@ -12,13 +15,12 @@ use clear_on_drop::clear::Clear;
 use hmac_drbg::HmacDRBG;
 use rust_scrypt::{scrypt, ScryptParams};
 use sha2::Sha512;
-use toml::Value;
 use typenum::U64;
 use unicode_segmentation::UnicodeSegmentation;
 
-const DEFAULT_LENGTH: usize = 24;
-
 const CHARSET_ALPHANUMERIC: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const DEFAULT_LENGTH: usize = 24;
 
 #[derive(Debug, PartialEq)]
 pub struct Password(String);
@@ -29,58 +31,36 @@ impl Drop for Password {
     }
 }
 
-#[derive(Debug)]
-pub struct PWM<'a> {
-    url: &'a str,
-    username: &'a str,
-    extra: Option<&'a str>,
-    prefix: &'a str,
-    charset: &'a str,
+#[serde(default)]
+#[derive(Debug, Deserialize)]
+pub struct PWM {
+    url: String,
+    username: String,
+    extra: Option<String>,
+    prefix: String,
+    charset: String,
     length: usize,
 }
 
-impl<'a> PWM<'a> {
-    pub fn from_value(value: &'a Value) -> Result<PWM<'a>, &'static str> {
-        let url = value
-            .get("url")
-            .and_then(|v| v.as_str())
-            .ok_or("no valid url")?;
-
-        let username = value
-            .get("username")
-            .and_then(|v| v.as_str())
-            .ok_or("no valid username")?;
-
-        let extra = value.get("extra").and_then(|v| v.as_str());
-
-        let prefix = value.get("prefix").and_then(|v| v.as_str()).unwrap_or("");
-
-        let length = value
-            .get("length")
-            .and_then(|v| v.as_integer())
-            .map(|i| i as usize)
-            .unwrap_or(DEFAULT_LENGTH);
-
-        let charset = value
-            .get("charset")
-            .and_then(|v| v.as_str())
-            .unwrap_or(CHARSET_ALPHANUMERIC);
-
-        Ok(PWM {
-            url,
-            username,
-            extra,
-            prefix,
-            charset,
-            length,
-        })
+impl Default for PWM {
+    fn default() -> Self {
+        PWM {
+            url: Default::default(),
+            username: Default::default(),
+            extra: None,
+            prefix: Default::default(),
+            charset: CHARSET_ALPHANUMERIC.to_string(),
+            length: DEFAULT_LENGTH,
+        }
     }
+}
 
+impl PWM {
     pub fn password(&self, key: &[u8]) -> Password {
         let mut drbg = HmacDRBG::<Sha512>::new(key, &[], &[]);
         drbg.reseed(self.url.as_bytes(), None);
         drbg.reseed(self.username.as_bytes(), None);
-        if let Some(extra) = self.extra {
+        if let Some(ref extra) = self.extra {
             drbg.reseed(extra.as_bytes(), None);
         }
 
@@ -135,126 +115,27 @@ impl Drop for Key {
 mod test {
     use super::*;
     use data_encoding::HEXLOWER;
-    use toml::Value::Table;
-
-    struct Test<'a> {
-        pwm: PWM<'a>,
-        pws: [&'a str; 2],
-    }
+    use toml::Value::{self, Table};
 
     const EXTRA: &str = "extra data";
 
     const PASSWORD_TEST_KEYS: [&str; 2] = ["", "secret key"];
 
-    const PASSWORD_TESTS: [Test; 7] = [
-        Test {
-            pwm: PWM {
-                url: "",
-                username: "",
-                extra: None,
-                prefix: "",
-                charset: CHARSET_ALPHANUMERIC,
-                length: 32,
-            },
-            pws: [
-                "8aRyuJhRGXAQFSlyngeSHpoVwjvKEBD9",
-                "TxuEPdxmb4ps3KMFFm7K9YpCsLn1Yati",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: None,
-                prefix: "",
-                charset: CHARSET_ALPHANUMERIC,
-                length: 32,
-            },
-            pws: [
-                "2b8CMXZJYVDGqEq8cjJLeI7Vd1tDiVbR",
-                "f67RwXJIQ9DtCTUcXyUogCzjO43TaY4V",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: None,
-                prefix: "",
-                charset: CHARSET_ALPHANUMERIC,
-                length: 48,
-            },
-            pws: [
-                "2b8CMXZJYVDGqEq8cjJLeI7Vd1tDiVbRRcmwf9gkjWIhdhgW",
-                "f67RwXJIQ9DtCTUcXyUogCzjO43TaY4VvonXCxjapWXiM9vW",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: None,
-                prefix: "foobar!",
-                charset: CHARSET_ALPHANUMERIC,
-                length: 32,
-            },
-            pws: [
-                "foobar!2b8CMXZJYVDGqEq8cjJLeI7Vd",
-                "foobar!f67RwXJIQ9DtCTUcXyUogCzjO",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: Some(EXTRA),
-                prefix: "",
-                charset: CHARSET_ALPHANUMERIC,
-                length: 32,
-            },
-            pws: [
-                "3UIXCUj9Lme0f17aNJI5sMBa6l0DzKi9",
-                "U1CdUb3gMyl2hpmIXZYLTQjQp16Sg9oX",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: None,
-                prefix: "",
-                charset: "αβγδεζηθικλμνξοπρστυφχψω",
-                length: 32,
-            },
-            pws: [
-                "ηθγρηωυδεχδλνινρψπδβηψμβυζπθαβσθ",
-                "μβχτσξγηδσχφκφιωγολιφχβονθωρονμσ",
-            ],
-        },
-        Test {
-            pwm: PWM {
-                url: "example.com",
-                username: "example@example.com",
-                extra: None,
-                prefix: "",
-                charset: "0⌘1",
-                length: 32,
-            },
-            pws: [
-                "0⌘1⌘01⌘0⌘00⌘010⌘⌘00⌘00⌘1⌘⌘10⌘0⌘1",
-                "1⌘⌘001⌘1001010111111⌘110⌘10⌘1⌘10",
-            ],
-        },
-    ];
+    macro_rules! test_url {
+        () => {
+            "example.com".to_string()
+        };
+    }
 
-    #[test]
-    fn test_passwords() {
-        for test in PASSWORD_TESTS.iter() {
-            for (k, expected) in test.pws.iter().enumerate() {
-                let actual = test.pwm.password(PASSWORD_TEST_KEYS[k].as_bytes());
-                assert_eq!(Password(expected.to_string()), actual);
-            }
-        }
+    macro_rules! test_username {
+        () => {
+            "example@example.com".to_string()
+        };
+    }
+
+    struct Test<'a> {
+        pwm: PWM,
+        pws: [&'a str; 2],
     }
 
     struct KeyTest<'a> {
@@ -262,48 +143,148 @@ mod test {
         keyhex: &'a [u8],
     }
 
-    const KEY_TESTS: [KeyTest; 9] = [
-        KeyTest {
-            passphrase: &[],
-            keyhex: b"cf4b3589438e51bfc0f942ca1f2b108d5a9e5a9238c15a2e76ab764484e636bd",
-        },
-        KeyTest {
-            passphrase: &[0u8],
-            keyhex: b"cf4b3589438e51bfc0f942ca1f2b108d5a9e5a9238c15a2e76ab764484e636bd",
-        },
-        KeyTest {
-            passphrase: &[1u8],
-            keyhex: b"cdbc42a4bf57aad0b0a4a86d3bb654e57bc356bd08d5de88a6548a3a031fc87e",
-        },
-        KeyTest {
-            passphrase: &[1u8, 0u8],
-            keyhex: b"cdbc42a4bf57aad0b0a4a86d3bb654e57bc356bd08d5de88a6548a3a031fc87e",
-        },
-        KeyTest {
-            passphrase: &[1u8, 0u8, 1u8],
-            keyhex: b"aac60e84780340d5e7065a27a7189e240f8777b1b7ac2144e9d9e4d93a599c53",
-        },
-        KeyTest {
-            passphrase: &[0u8, 1u8],
-            keyhex: b"a35b6569ec9ac21d16c43db825436e92b5a23b6288e17503664962f148e72101",
-        },
-        KeyTest {
-            passphrase: &[0u8, 0u8, 1u8],
-            keyhex: b"df71d36f29d13211d9f74e77828cdc1c83e41a3a5407bc231bdca2d1504b1544",
-        },
-        KeyTest {
-            passphrase: b"passphrase",
-            keyhex: b"40f2dacf5fdb770dc6e047f41883ff71ec3972aa7ac92d1792dd2909f2453324",
-        },
-        KeyTest {
-            passphrase: b"another passphrase",
-            keyhex: b"2aaf56826d42fdcbe6ae5653f50b10fc47748d8c2b3e36515bb01078c7c8f535",
-        },
-    ];
+    #[test]
+    fn test_passwords() {
+        let password_tests: [Test; 7] = [
+            Test {
+                pwm: PWM {
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "8aRyuJhRGXAQFSlyngeSHpoVwjvKEBD9",
+                    "TxuEPdxmb4ps3KMFFm7K9YpCsLn1Yati",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "2b8CMXZJYVDGqEq8cjJLeI7Vd1tDiVbR",
+                    "f67RwXJIQ9DtCTUcXyUogCzjO43TaY4V",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    length: 48,
+                    ..Default::default()
+                },
+                pws: [
+                    "2b8CMXZJYVDGqEq8cjJLeI7Vd1tDiVbRRcmwf9gkjWIhdhgW",
+                    "f67RwXJIQ9DtCTUcXyUogCzjO43TaY4VvonXCxjapWXiM9vW",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    prefix: "foobar!".to_string(),
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "foobar!2b8CMXZJYVDGqEq8cjJLeI7Vd",
+                    "foobar!f67RwXJIQ9DtCTUcXyUogCzjO",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    extra: Some(EXTRA.to_string()),
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "3UIXCUj9Lme0f17aNJI5sMBa6l0DzKi9",
+                    "U1CdUb3gMyl2hpmIXZYLTQjQp16Sg9oX",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    extra: None,
+                    charset: "αβγδεζηθικλμνξοπρστυφχψω".to_string(),
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "ηθγρηωυδεχδλνινρψπδβηψμβυζπθαβσθ",
+                    "μβχτσξγηδσχφκφιωγολιφχβονθωρονμσ",
+                ],
+            },
+            Test {
+                pwm: PWM {
+                    url: test_url!(),
+                    username: test_username!(),
+                    charset: "0⌘1".to_string(),
+                    length: 32,
+                    ..Default::default()
+                },
+                pws: [
+                    "0⌘1⌘01⌘0⌘00⌘010⌘⌘00⌘00⌘1⌘⌘10⌘0⌘1",
+                    "1⌘⌘001⌘1001010111111⌘110⌘10⌘1⌘10",
+                ],
+            },
+        ];
+
+        for test in password_tests.iter() {
+            for (k, expected) in test.pws.iter().enumerate() {
+                let actual = test.pwm.password(PASSWORD_TEST_KEYS[k].as_bytes());
+                assert_eq!(Password(expected.to_string()), actual);
+            }
+        }
+    }
 
     #[test]
     fn test_keys() {
-        for test in KEY_TESTS.iter() {
+        let key_tests: [KeyTest; 9] = [
+            KeyTest {
+                passphrase: &[],
+                keyhex: b"cf4b3589438e51bfc0f942ca1f2b108d5a9e5a9238c15a2e76ab764484e636bd",
+            },
+            KeyTest {
+                passphrase: &[0u8],
+                keyhex: b"cf4b3589438e51bfc0f942ca1f2b108d5a9e5a9238c15a2e76ab764484e636bd",
+            },
+            KeyTest {
+                passphrase: &[1u8],
+                keyhex: b"cdbc42a4bf57aad0b0a4a86d3bb654e57bc356bd08d5de88a6548a3a031fc87e",
+            },
+            KeyTest {
+                passphrase: &[1u8, 0u8],
+                keyhex: b"cdbc42a4bf57aad0b0a4a86d3bb654e57bc356bd08d5de88a6548a3a031fc87e",
+            },
+            KeyTest {
+                passphrase: &[1u8, 0u8, 1u8],
+                keyhex: b"aac60e84780340d5e7065a27a7189e240f8777b1b7ac2144e9d9e4d93a599c53",
+            },
+            KeyTest {
+                passphrase: &[0u8, 1u8],
+                keyhex: b"a35b6569ec9ac21d16c43db825436e92b5a23b6288e17503664962f148e72101",
+            },
+            KeyTest {
+                passphrase: &[0u8, 0u8, 1u8],
+                keyhex: b"df71d36f29d13211d9f74e77828cdc1c83e41a3a5407bc231bdca2d1504b1544",
+            },
+            KeyTest {
+                passphrase: b"passphrase",
+                keyhex: b"40f2dacf5fdb770dc6e047f41883ff71ec3972aa7ac92d1792dd2909f2453324",
+            },
+            KeyTest {
+                passphrase: b"another passphrase",
+                keyhex: b"2aaf56826d42fdcbe6ae5653f50b10fc47748d8c2b3e36515bb01078c7c8f535",
+            },
+        ];
+
+        for test in key_tests.iter() {
             let actual: Vec<u8> = Key::new(test.passphrase).into();
             let expected = HEXLOWER.decode(test.keyhex).unwrap();
             assert_eq!(expected, actual);
@@ -316,23 +297,18 @@ mod test {
             [example]
             url = 'example.com'
             username = 'example@example.com'
+
+            [server]
+            url = 'server.com'
+            username = 'server@server.com'
+            prefix = 'quux'
         "#;
 
         if let Table(parsed) = config.parse::<Value>().unwrap() {
-            println!("{:?}", parsed);
-
-            let res = {
-                let mut ret = vec![];
-                for (_, value) in &parsed {
-                    let pwm = PWM::from_value(&value).unwrap();
-                    ret.push(pwm);
-                }
-                ret
-            };
-
-            println!("{:?}", res);
+            for (key, value) in parsed {
+                let pwm: PWM = value.try_into().unwrap();
+                println!("{}: {:?}", key, pwm);
+            }
         }
-
-        // assert_eq!(pwm.url, "example.com");
     }
 }
